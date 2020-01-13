55d4cc2c1fc42e6457153a196d38d7bafed8f6bc
Absner Anaya <absrraniel@gmail.com>
2020-01-12 23:31:25 -0400

    cambios

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/leangasftware'));
+app.use(express.static('./dist/leangasoftware'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/leangasftware/index.html'));
+res.sendFile(path.join(__dirname,'/dist/leangasoftware/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
